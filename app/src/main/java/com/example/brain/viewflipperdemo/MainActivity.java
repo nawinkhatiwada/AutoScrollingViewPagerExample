632e6e8f2fd66d0097a4647621e4ad056ae2aa13
nawinkhatiwada <nabin.khatiwada@braindigit.com>
2017-11-27 17:36:05 +0545

    code refactored

--- app/src/main/java/com/example/brain/viewflipperdemo/MainActivity.java
+++ app/src/main/java/com/example/brain/viewflipperdemo/MainActivity.java
@@ -4,47 +4,42 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.viewpagerindicator.CirclePageIndicator;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-    private static ViewPager mPager;
+    private ViewPager mPager;
     private static int currentPage = 0;
     private static int NUM_PAGES = 0;
-    private static final Integer[] IMAGES= {R.drawable.three,R.drawable.two,R.drawable.three,R.drawable.one};
-    private ArrayList<Integer> ImagesArray = new ArrayList<Integer>();
+    private static final Integer[] IMAGES = {R.drawable.three, R.drawable.two, R.drawable.three, R.drawable.one};
+    private ArrayList<Integer> ImagesArray = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         init();
     }
+
     private void init() {
-        for(int i=0;i<IMAGES.length;i++)
-            ImagesArray.add(IMAGES[i]);
+        ImagesArray.addAll(Arrays.asList(IMAGES));
 
-        mPager = (ViewPager) findViewById(R.id.pager);
-
-
-        mPager.setAdapter(new SlidingImage_Adapter(MainActivity.this,ImagesArray));
-
-
-        CirclePageIndicator indicator = (CirclePageIndicator)
-                findViewById(R.id.indicator);
-
+        mPager = findViewById(R.id.pager);
+        mPager.setAdapter(new SlidingImage_Adapter(MainActivity.this, ImagesArray));
+        CirclePageIndicator indicator = findViewById(R.id.indicator);
         indicator.setViewPager(mPager);
-
         final float density = getResources().getDisplayMetrics().density;
-
-//Set circle indicator radius
+        //Set circle indicator radius
         indicator.setRadius(5 * density);
-
-        NUM_PAGES =IMAGES.length;
-
+        NUM_PAGES = IMAGES.length;
         // Auto start of viewpager
         final Handler handler = new Handler();
         final Runnable Update = new Runnable() {
@@ -64,25 +59,12 @@ public class MainActivity extends AppCompatActivity {
         }, 3000, 3000);
 
         // Pager listener over indicator
-        indicator.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-
+        indicator.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
             @Override
             public void onPageSelected(int position) {
                 currentPage = position;
-
-            }
-
-            @Override
-            public void onPageScrolled(int pos, float arg1, int arg2) {
-
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int pos) {
-
             }
         });
 
     }
-
 }
